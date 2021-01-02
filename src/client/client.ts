c9444edcfacd609b836f4712a966173b657d18f7
jaustinmiles <j.austinmiles@outlook.com>
2021-01-02 14:20:03 -0500

    add final server and client setup with import paths

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,11 +1,22 @@
 
+import * as THREE from '/build/three.module.js'
+import {OrbitControls} from '/jsm/controls/OrbitControls'
+
 const scene: THREE.Scene = new THREE.Scene()
 
-const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera1: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, 200 / 200, 0.1, 1000)
+const camera2: THREE.OrthographicCamera = new THREE.OrthographicCamera(-2, 2, 2, -2, 0.1, 10);
 
-const renderer: THREE.WebGLRenderer = new THREE.WebGLRenderer()
-renderer.setSize(window.innerWidth, window.innerHeight)
-document.body.appendChild(renderer.domElement)
+const canvas1: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('c1');
+const canvas2: HTMLCanvasElement = <HTMLCanvasElement>document.getElementById('c2');
+const renderer1: THREE.WebGLRenderer = new THREE.WebGLRenderer({canvas:canvas1})
+const renderer2: THREE.WebGLRenderer = new THREE.WebGL1Renderer({canvas: canvas2})
+renderer1.setSize(600, 600)
+renderer2.setSize(600, 600)
+document.body.appendChild(renderer1.domElement)
+document.body.appendChild(renderer2.domElement)
+
+const controls: OrbitControls = new OrbitControls(camera1, renderer1.domElement); 
 
 const geometry: THREE.BoxGeometry = new THREE.BoxGeometry()
 const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
@@ -13,7 +24,8 @@ const material: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({ color: 0
 const cube: THREE.Mesh = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
-camera.position.z = 2
+camera1.position.z = 2
+camera2.position.z = 2
 
 var animate = function () {
     requestAnimationFrame(animate)
@@ -21,7 +33,10 @@ var animate = function () {
     cube.rotation.x += 0.01;
     cube.rotation.y += 0.01;
 
-    renderer.render(scene, camera)
+    controls.update();
+
+    renderer1.render(scene, camera1)
+    renderer2.render(scene, camera2)
 };
 
 animate();
